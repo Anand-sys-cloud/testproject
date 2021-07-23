a07fdfb8d4ba67d2892b965b46c6ab31e4eadceb
lenovo <lenovo@DESKTOP-SJHS2F3>
2021-07-23 22:07:47 +0530

    jdbc

--- COD/src/com/kod/sales/hello.java
+++ COD/src/com/kod/sales/hello.java
@@ -1,5 +1,37 @@
 package com.kod.sales;
 
-public class hello {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class hello
+
+{
+ 
+	public static void main(String args[])
+	{
+		String url="jdbc:mysql://localhost:3306/";
+		String dbname="emp";
+		String uname="root";
+		String pwd="root";
+		
+		try
+		{
+			Connection con=DriverManager.getConnection(url+dbname,uname,pwd);
+			
+			System.out.println("connection  established");
+			
+			
+			
+			
+		}
+		
+		catch(SQLException e)
+		{
+			System.out.println("Exception handled");
+		}
+	}
+	
+	
+	
 }
