31e2673071c7b2c4f9150730359400c7d5bf00bb
lenovo <lenovo@DESKTOP-SJHS2F3>
2021-07-23 22:19:28 +0530

    update

--- COD/src/com/kod/sales/hello.java
+++ COD/src/com/kod/sales/hello.java
@@ -2,7 +2,9 @@ package com.kod.sales;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class hello
 
@@ -21,8 +23,22 @@ public class hello
 			
 			System.out.println("connection  established");
 			
+			Statement stmt=con.createStatement();
+			String update="update emp.sales set name= 'jivan' where id=2";
+			stmt.execute(update);
+			
+			String select ="select * from emp.sales";
+			ResultSet res=stmt.executeQuery(select);
 			
 			
+			while(res.next()==true)
+			{
+				int id=res.getInt(1);
+				String name=res.getString(2);
+				String place=res.getString(3);
+				System.out.println(id +" " + name);
+				
+			}
 			
 		}
 		
